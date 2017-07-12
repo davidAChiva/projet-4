a64f463f8c0ecbf5e17df1342014c74b81fa3a75
davidAChiva <david.alfaro.chiva@gmail.com>
2017-07-12 15:12:35 +0200

    Correction du fichier template.php et ajout d'une image

---
+++ Vue/template.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <title><?= $titre ?></title>
+    </head>
+    <body>
+        <header>
+            <div id="header1">
+               <h1>BILLET SIMPLE POUR L'ALASKA</h1>
+               <h2>Livre de Jean Forteroche</h2>
+            </div>
+            <div id="header2">
+                <img src="../images/aventure-alaska-camping.jpg" alt="Image d'un camping sauvage en Alaska">
+            </div>
+        </header>
+        <section id="list_episodes">
+        <?= $corpsPage ?>    
+        </section>
+        <footer>
+            <h4>BLOG REALISE PAR DAVID ALFARO-CHIVA POUR MONSIEUR JEAN FORTEROCHE</h4>
+        </footer>
+    </body>
+</html>
