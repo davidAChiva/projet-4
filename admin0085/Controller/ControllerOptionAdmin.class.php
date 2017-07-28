416bf7e2925c7c1e5d4b85e0bcffb86357d9c6c4
davidAChiva <david.alfaro.chiva@gmail.com>
2017-07-28 15:39:34 +0200

    Rajout du controller option admin sur git

--- admin0085/Controller/ControllerOptionAdmin.class.php
+++ admin0085/Controller/ControllerOptionAdmin.class.php
@@ -1,18 +1,30 @@
 <?php
+
+require_once 'Model/OptionAdmin.class.php';
+
 class ControllerOptionAdmin
 {
     private $getOption;
+    private $setEpisode;
     
     public function __construct ($option)
     {
         $this->getOption = $option;
+        $this->setEpisode = new OptionAdmin;
     }
     
-    function displayOption ()
+    // Affiche la page demandée
+    public function displayOption ()
     {
         if ($this->getOption === 'newEpisode')
         {
             require_once 'View/ViewCreateEpisode.php';
         }
+    }
+    
+    // Creer le nouvel épisode dans la base de donnée
+    public function setEpisode($title, $content)
+    {
+        $this->setEpisode->setEpisode($title,$content);
     }
 }
