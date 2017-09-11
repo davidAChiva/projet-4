d2c6084bddbda01e28d0f7410d7f772314006c18
davidAChiva <david.alfaro.chiva@gmail.com>
2017-09-11 17:17:04 +0200

    Amélioration design

--- admin0085/View/ViewModifyComment.php
+++ admin0085/View/ViewModifyComment.php
@@ -5,8 +5,10 @@ ob_start();
 <h2>MODERER LE COMMENTAIRE</h2>
 <form method='post' action='home.php?rubric=manageComments&idEpisode=<?= $comment['id'] ?>&typeManage=modify&idComment=<?= $comment["id"] ?>'>
     <input type='text' id='idComment' name='idComment' value='<?= $comment['id'] ?>'hidden></input>
+<h3><label for='authorComment'>Auteur du commentaire</label></h3>
     <input type='text' id='authorComment' name='authorComment' value='<?= $comment['author'] ?>'></input>
-    <textarea id='contentComment' name='contentComment' rows='10'><?= $comment['content'] ?></textarea>
+<h3><label for='contentComment'>Contenu du commentaire</label></h3>
+    <textarea id='contentComment' name='contentComment' rows='10' cols='60'><?= $comment['content'] ?></textarea>
     <input class='submit'type='submit' value='Modifier' </input>
 </form>
 
