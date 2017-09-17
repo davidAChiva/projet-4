9980a6bad510a292c18d8d5aa845c486ef740163
davidAChiva <david.alfaro.chiva@gmail.com>
2017-09-17 18:08:43 +0200

    Design connexion administration

--- admin0085/View/ViewConnexion.php
+++ admin0085/View/ViewConnexion.php
@@ -2,15 +2,18 @@
 <html lang='fr'>
     <head>
         <meta charset='UTF-8' />
-        <link rel='stylesheet' href='../css/generalBack.css' />
+        <link rel='stylesheet' href='../css/general.css' />
         <link rel='stylesheet' href='../css/back.css' />
         <title>Connexion Admin | Billet simple pour l'Alaska</title>
     </head>
     <body>
+        <header>
+        <h1>ADMINISTRATION | BILLET POUR L'ALASKA</h1>
+        </header>
         <form method='post' action='index.php'>
-            <label for='pseudoAdmin'> Pseudo</label>
+            <label for='pseudoAdmin'>PSEUDO</label>
             <input type='text' id='pseudoAdmin' name='pseudoAdmin' required /> <br />
-            <label for='passwordAdmin'>Votre mot de passe</label>
+            <label for='passwordAdmin'>MOT DE PASSE</label>
             <input type='password' id='passwordAdmin' name='passwordAdmin' required /> <br />
             <input class='submit' type='submit' value='Envoyer' />
         </form>
